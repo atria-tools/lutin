9d5d0ca041bc40f7eba950a808900f5031efcf48
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-07-12 06:50:54 +0200

    [DEV] enable abstraction file...

--- lutinTargetAndroid.py
+++ lutinTargetAndroid.py
@@ -97,13 +97,13 @@ class Target(lutinTarget.Target):
 		#FINAL_FILE_ABSTRACTION
 		self.file_finalAbstraction = self.folder_javaProject + "/" + pkgName + ".java"
 		
-		#lutinTools.CopyFile(self.folder_ewol + "/sources/android/PROJECT_NAME.java", self.file_finalAbstraction, True)
+		lutinTools.CopyFile(self.folder_ewol + "/sources/android/PROJECT_NAME.java", self.file_finalAbstraction, True)
 		
-		#os.system("sed -i \"s|__PROJECT_ORG_TYPE__|"+pkgProperties["COMPAGNY_TYPE"]+"|\" " + self.file_finalAbstraction)
-		#os.system("sed -i \"s|__PROJECT_VENDOR__|"+pkgProperties["COMPAGNY_NAME2"]+"|\" " + self.file_finalAbstraction)
-		#os.system("sed -i \"s|__PROJECT_NAME__|" + pkgName + "|\" "+ self.file_finalAbstraction)
-		#os.system("sed -i \"s|__PROJECT_PACKAGE__|" + pkgName + "|\" " + self.file_finalAbstraction)
-		#os.system("sed -i \"s|__CONF_OGL_ES_V__|2|\" " + self.file_finalAbstraction)
+		os.system("sed -i \"s|__PROJECT_ORG_TYPE__|"+pkgProperties["COMPAGNY_TYPE"]+"|\" " + self.file_finalAbstraction)
+		os.system("sed -i \"s|__PROJECT_VENDOR__|"+pkgProperties["COMPAGNY_NAME2"]+"|\" " + self.file_finalAbstraction)
+		os.system("sed -i \"s|__PROJECT_NAME__|" + pkgName + "|\" "+ self.file_finalAbstraction)
+		os.system("sed -i \"s|__PROJECT_PACKAGE__|" + pkgName + "|\" " + self.file_finalAbstraction)
+		os.system("sed -i \"s|__CONF_OGL_ES_V__|2|\" " + self.file_finalAbstraction)
 		
 		lutinTools.CopyFile(pkgProperties["ICON"], self.GetStagingFolder(pkgName) + "/res/drawable/icon.png", True)
 		
@@ -192,6 +192,7 @@ class Target(lutinTarget.Target):
 		          + self.folder_ewol + "/sources/android/src/org/ewol/EwolRendererGL.java " \
 		          + self.folder_ewol + "/sources/android/src/org/ewol/EwolSurfaceViewGL.java " \
 		          + self.folder_ewol + "/sources/android/src/org/ewol/EwolActivity.java " \
+		          + self.file_finalAbstraction + " "  \
 		          + self.GetStagingFolder(pkgName) + "/src/R.java "
 		lutinMultiprocess.RunCommand(cmdLine)
 		
