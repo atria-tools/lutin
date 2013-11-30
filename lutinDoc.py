50caa0371f1c01e477488335da7ab01fc66a1c1e
Edouard DUPIN <yui.heero@gmail.com>
2013-11-30 18:34:20 +0100

    [DEV] add ply and cppHeaderParser and basic markdown generato of doc

--- lutinDoc.py
+++ lutinDoc.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import lutinDebug as debug
 import sys
-import CppHeaderParser
 import lutinTools
+# TODO : Add try of generic input ...
+sys.path.append(lutinTools.GetCurrentPath(__file__) + "/ply/ply/")
+sys.path.append(lutinTools.GetCurrentPath(__file__) + "/cppParser/CppheaderParser/")
+import CppHeaderParser
 
 def writeExpendSize(data, size) :
 	ret = data
@@ -10,25 +13,25 @@ def writeExpendSize(data, size) :
 		ret += " "
 	return ret
 
-def displayReductFunction(className, function, file, classement, sizeReturn, sizefunction) :
-	lineData = "\t" + classement + " "
+def displayReductFunction(function, file, classement, sizeReturn, sizefunction) :
+	lineData = classement + " "
 	
 	if function['destructor'] :
-		lineData += writeExpendSize("", sizeReturn+1)
-	elif function['constructor'] :
 		lineData += writeExpendSize("", sizeReturn)
 		lineData += "~"
+	elif function['constructor'] :
+		lineData += writeExpendSize("", sizeReturn+1)
 	else :
 		lineData += writeExpendSize(function["rtnType"], sizeReturn+1)
 	
 	lineData += writeExpendSize(function["name"], sizefunction+1)
 	lineData += "("
-	file.write(lineData);
+	file.write("\t" + lineData);
 	parameterPos = len(lineData);
 	isFirst = True
 	for param in function["parameters"]:
 		if isFirst == False:
-			file.write(",\n")
+			file.write(",\n\t")
 			file.write(writeExpendSize("",parameterPos))
 		file.write(param['type'])
 		if param['name'] != "":
@@ -37,6 +40,52 @@ def displayReductFunction(className, function, file, classement, sizeReturn, siz
 		isFirst = False
 	file.write(");")
 	file.write("\n")
+
+
+def displayFunction(namespace, function, file, classement, sizeReturn, sizefunction) :
+	lineData = ""
+	if namespace != "":
+		lineData = namespace + "::"
+	if function['destructor'] :
+		lineData += "~"
+	lineData += function["name"] + "( ... )"
+	file.write(lineData + "\n")
+	for iii in range(0, len(lineData)):
+		file.write(".")
+	file.write("\n\n")
+
+	if function['destructor'] :
+		lineData = "~"
+	elif function['constructor'] :
+		lineData = ""
+	else :
+		lineData = function["rtnType"] + " "
+
+	lineData += function["name"]
+	lineData += "("
+	file.write("\t" + lineData);
+	parameterPos = len(lineData);
+	isFirst = True
+	for param in function["parameters"]:
+		if isFirst == False:
+			file.write(",\n\t")
+			file.write(writeExpendSize("",parameterPos))
+		file.write(param['type'])
+		if param['name'] != "":
+			file.write(" ")
+			file.write(param['name'])
+		isFirst = False
+	file.write(");")
+	file.write("\n\n")
+	if "doxygen" in function:
+		# TODO : parse doxygen ...
+		file.write(function["doxygen"])
+		file.write("\n")
+	file.write("\n")
+
+
+
+
 
 def calsulateSizeFunction(function, size) :
 	if len(function["name"]) > size:
@@ -58,7 +107,7 @@ def GenerateDocFile(filename, outFolder) :
 	lutinTools.CreateDirectoryOfFile(outFolder+"/");
 	
 	for element in metaData.classes:
-		classFileName = outFolder + "/";
+		classFileName = outFolder + "/"
 		localClass = metaData.classes[element]
 		if localClass['namespace'] == "":
 			className = localClass['name']
@@ -96,15 +145,29 @@ def GenerateDocFile(filename, outFolder) :
 		# display all functions :
 		# TODO: ...
 		for function in localClass["methods"]["public"]:
-			displayReductFunction(localClass['name'], function, file, "public:   ", sizeReturn, sizefunction)
+			displayReductFunction(function, file, "public:   ", sizeReturn, sizefunction)
 		for function in localClass["methods"]["protected"]:
-			displayReductFunction(localClass['name'], function, file, "protected:", sizeReturn, sizefunction)
+			displayReductFunction(function, file, "protected:", sizeReturn, sizefunction)
 		for function in localClass["methods"]["private"]:
-			displayReductFunction(localClass['name'], function, file, "private:  ", sizeReturn, sizefunction)
+			displayReductFunction(function, file, "private:  ", sizeReturn, sizefunction)
 		
 		file.write("\n")
 		file.write("\n")
-		
+
+
+		if len(localClass['inherits']) != 0:
+			file.write("Object Hierarchy:\n")
+			file.write("-----------------\n")
+			file.write("\n")
+			for heritedClass in localClass['inherits']:
+				file.write("\t" + heritedClass['class'] + "\n")
+			file.write("\t    |\n")
+			file.write("\t    +--> " +  localClass['name'] + "\n")
+			file.write("\n")
+			file.write("\n")
+
+
+		"""
 		file.write("Signals:\n")
 		file.write("--------\n")
 		file.write("\n")
@@ -116,22 +179,34 @@ def GenerateDocFile(filename, outFolder) :
 		file.write("\n")
 		# display all configuration :
 		# TODO: ...
+		"""
 		
-		file.write("Description:\n")
-		file.write("------------\n")
-		file.write("\n")
-		# display Class description :
-		# TODO: ...
+		if "doxygen" in localClass:
+			file.write("Description:\n")
+			file.write("------------\n")
+			file.write("\n")
+			# display Class description :
+			file.write(localClass["doxygen"])
+			file.write("\n")
+			file.write("\n")
 		
 		
 		file.write("Detail:\n")
 		file.write("-------\n")
 		file.write("\n")
 		# display all the class internal functions :
-		# TODO: ...
-		
-		
-		
+		for function in localClass["methods"]["public"]:
+			displayFunction(localClass['namespace'] , function, file, "public:   ", sizeReturn, sizefunction)
+			file.write("\n________________________________________________________________________\n\n")
+		for function in localClass["methods"]["protected"]:
+			displayFunction(localClass['namespace'] , function, file, "protected:", sizeReturn, sizefunction)
+			file.write("\n________________________________________________________________________\n\n")
+		for function in localClass["methods"]["private"]:
+			displayFunction(localClass['namespace'] , function, file, "private:  ", sizeReturn, sizefunction)
+			file.write("\n________________________________________________________________________\n\n")
+
+
+
 		if len(localClass['inherits']) != 0:
 			for heritedClass in localClass['inherits']:
 				debug.debug("        heritage : " + str(heritedClass['class']))
