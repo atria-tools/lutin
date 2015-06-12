9034d303e5d78bd8460508c5ab0ca11321b9fbe2
Edouard DUPIN <yui.heero@gmail.com>
2015-06-12 10:14:50 +0200

    [DEBUG] correct the inclusion of many element

--- setup.py
+++ setup.py
@@ -7,18 +7,19 @@ def readme():
 
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 setup(name='lutin',
-      version='0.5.8',
+      version='0.5.9',
       description='Lutin generic builder',
       long_description=readme(),
       url='http://github.com/HeeroYui/lutin',
       author='Edouard DUPIN',
       author_email='yui.heero@gmail.com',
       license='APACHE-2',
-      packages=['lutin',
-                'lutin/builder',
-                'lutin/system',
-                'lutin/target'
-               ],
+      packages=['lutin'],
+      package_data={
+          'lutin.builder': ['lutin/builder/*'],
+          'lutin.system': ['lutin/system/*'],
+          'lutin.target': ['lutin/target/*']
+      },
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
@@ -30,6 +31,9 @@ setup(name='lutin',
       include_package_data = True,
       data_file=[
           ('/etc/bash_completion.d', ['bash-autocompletion/lutin']),
+      ],
+      install_requires=[
+          'PIL>=1.0.0'
       ],
       zip_safe=False)
 
