4d82e31d40ac945be42a4ff617a8fb4e3a42edf4
Edouard DUPIN <yui.heero@gmail.com>
2015-10-20 23:39:12 +0200

    [TAG] creata a new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 setup(name='lutin',
-      version='0.7.4',
+      version='0.7.5',
       description='Lutin generic builder',
       long_description=readme(),
       url='http://github.com/HeeroYui/lutin',
