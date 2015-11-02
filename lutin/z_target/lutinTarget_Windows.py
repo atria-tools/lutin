b497e09dd000c85df30a0539dcec215416c4b649
Edouard DUPIN <yui.heero@gmail.com>
2015-11-02 21:55:32 +0100

    [CI] set minGW builder better

--- lutin/z_target/lutinTarget_Windows.py
+++ lutin/z_target/lutinTarget_Windows.py
@@ -39,23 +39,16 @@ class Target(target.Target):
 			sys.path.append("c:\\MinGW\\bin" )
 			os.environ['PATH'] += ";c:\\MinGW\\bin\\"
 		else:
-			#if self.config["bus-size"] == "64":
-			#	# 64 bits
-			#	self.set_cross_base("x86_64-w64-mingw32-")
-			#else:
-			#	# 32 bits
-			#	self.set_cross_base("i686-w64-mingw32-")
-			# Only one ... need understand why
-			self.set_cross_base("i686-w64-mingw32-")
+			if self.config["bus-size"] == "64":
+				# 64 bits
+				self.set_cross_base("x86_64-w64-mingw32-")
+			else:
+				# 32 bits
+				self.set_cross_base("i686-w64-mingw32-")
 		# force static link to prenvent many errors ...
 		self.global_flags_ld.append(["-static-libgcc",
 		                             "-static-libstdc++",
 		                             "-static"])
-		
-		#self.path_bin=""
-		#self.path_lib="lib"
-		#self.path_data="data"
-		#self.path_doc="doc"
 		
 		self.pkg_path_data = "data"
 		self.pkg_path_bin = ""
