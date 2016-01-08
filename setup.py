055a37bcd51aa9ba15c39ab7c9245301e4f811d8
Edouard DUPIN <yui.heero@gmail.com>
2016-01-08 22:33:51 +0100

    [RELEASE] Change API ==> change master version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 setup(name='lutin',
-      version='0.7.10',
+      version='1.0.0',
       description='Lutin generic builder (might replace makefile, CMake ...)',
       long_description=readme(),
       url='http://github.com/HeeroYui/lutin',
