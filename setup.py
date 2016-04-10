520f97c7f65cf345631a2361063895f908db41d5
Edouard DUPIN <yui.heero@gmail.com>
2016-04-10 13:39:36 +0200

    [RELEASE] create version 1.2.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 setup(name='lutin',
-      version='1.2.1',
+      version='1.2.2',
       description='Lutin generic builder (might replace makefile, CMake ...)',
       long_description=readme(),
       url='http://github.com/HeeroYui/lutin',
