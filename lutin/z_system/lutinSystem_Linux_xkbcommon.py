791c7fe6e8c298d80cf5d98e3196b2cc28174735
Edouard DUPIN <yui.heero@gmail.com>
2017-01-17 22:16:53 +0100

    [DEV] add xdg compose

---
+++ lutin/z_system/lutinSystem_Linux_xkbcommon.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##
+## @author Edouard DUPIN
+##
+## @copyright 2012, Edouard DUPIN, all right reserved
+##
+## @license MPL v2.0 (see license file)
+##
+
+from lutin import debug
+from lutin import system
+from lutin import tools
+from lutin import env
+import os
+
+class System(system.System):
+	def __init__(self, target):
+		system.System.__init__(self)
+		# create some HELP:
+		self.set_help("XKB common: common wrapping of keyborad layout")
+		self.set_valid(True)
+		# no check needed ==> just add this:
+		self.add_depend(['c'])
+		self.add_flag('link-lib', 'xkbcommon')
+		if env.get_isolate_system() == True:
+			self.add_header_file([
+			    "/usr/include/xkbcommon/*"
+			    ],
+			    destination_path="xkbcommon",
+			    recursive=True)
+
+
