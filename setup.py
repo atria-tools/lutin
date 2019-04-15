d3be28a1a351107c5188af884aedfc75c4498629
Edouard DUPIN <yui.heero@gmail.com>
2019-04-15 22:41:41 +0200

    [RELEASE] new version 2.4.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 setup(name='lutin',
-      version='2.3.0',
+      version='2.4.0',
       description='Lutin generic builder (might replace makefile, CMake ...)',
       long_description=readme(),
       url='http://github.com/HeeroYui/lutin',
